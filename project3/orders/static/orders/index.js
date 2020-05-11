004d6ce1367b6cf5ab98178b803a12d9499b86f9
Ilia Gurov <feraijo@gmail.com>
2020-05-11 20:23:15 +0500

    order alert

--- project3/orders/static/orders/index.js
+++ project3/orders/static/orders/index.js
@@ -1,8 +1,47 @@
 document.addEventListener('DOMContentLoaded', () => {
+    var adds = []
 
-    document.querySelector('button[data-dismiss].btn.btn-secondary').onclick = () => {check_form()}
+    document.querySelector('#purch_form').onsubmit = function(){
+        //var data = new FormData(this)
+        inp_id = document.querySelector('#inp_id').value
+        var prod_type = document.querySelector('div.modal-body2').dataset.prod_type.trim()
+        tops = document.querySelector('div.modal-body2').dataset.toppings
+        if (tops && adds.length < tops){
+            alert("Choose your toppings!")
+            return false;
+        }
+        data = {
+            'type': prod_type,
+            'id': inp_id, 
+            'adds':adds,
+        }
+        alert(JSON.stringify(data))        
+        return false;
+    }
+
+    document.querySelectorAll('.closebut').forEach(but => {
+        but.onclick = () => {adds = check_form(adds)}
+    })
     
-    
+    document.querySelectorAll('.custom-control-input.checkbox').forEach(check => {
+        check.onchange = () => {   
+            if (check.checked){
+                var prod_type = document.querySelector('div.modal-body2').dataset.prod_type
+                if (prod_type.trim() === 'Pizza'){
+                    var tops = document.querySelector('div.modal-body2').dataset.toppings                    
+                    if (isEmpty(tops) || adds.length >= tops){
+                            check.checked = false                    
+                    } else {
+                        adds.push(check.id)
+                    }
+                } else 
+                    adds.push(check.id)
+            } else {
+                adds.pop(check.id)
+            }
+        }
+    })
+
     document.querySelectorAll('.card').forEach(ch => {
         ch.addEventListener('mouseover', () => { 
             //alert(1)
@@ -16,35 +55,37 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 
     document.querySelectorAll('.cart_add').forEach(button => {
-        button.onclick = function(){
+        button.onclick = function(){            
             var size = ''
-            this.parentElement.querySelectorAll('.custom-control-input').forEach(x => {
-                if (x.checked){
+            this.parentElement.querySelectorAll('.custom-control-input.radio').forEach(x => {
+                if (x.checked){                                      
                     size = x.id.split('_')[0]
                     document.querySelector('.modal-body p').innerHTML = 'Price: $' + x.value;
+                    document.querySelector('#inp_id').value = x.id;
                     //query_for_array()
                 }
                 x.checked = false
             })
-            card = this.parentElement.parentElement
+            prod_type = document.querySelector('h1').innerHTML
+            card = this.parentElement
             document.querySelector('.modal-title').innerHTML = capitalize(size)+ ' ' + 
-                card.querySelector('.card-title').innerHTML + 
-                ' ' + document.querySelector('h1').innerHTML
-            document.querySelector('.modal-title').dataset.toppings = card.dataset.toppings
+                card.querySelector('.card-title').innerHTML + ' ' + prod_type
+            document.querySelector('div.modal-body2').dataset.toppings = card.dataset.toppings
+            document.querySelector('div.modal-body2').dataset.prod_type = prod_type
         }
     })   
 })
 
-function check_form() {
-    tops = document.querySelector('.modal-title').dataset.toppings
-    if (tops){
-        alert(1)
-    }
+function isEmpty(str) {
+    return (!str || 0 === str.length);
+}
+
+function check_form(adds) {
     document.querySelectorAll('div.modal-body2 input').forEach(check => {
-        check.checked = false
-        //card.dataset.toppings
+        check.checked = false        
     })
-    
+    adds = []
+    return adds
 }
 
 function capitalize(string){
